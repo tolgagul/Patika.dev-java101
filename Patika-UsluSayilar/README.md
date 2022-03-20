d701c41e4efb4fe9b2b9f9c3de6a97097fa68f07
Tolga Gül <tolgagl19@gmail.com>
2022-03-20 16:18:14 +0300

    Initial commit

---
+++ Patika-UsluSayilar/README.md
@@ -0,0 +1,4 @@
+## Üslü Sayı Hesaplayan Program
+
+Ödev  
+Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan programı "For Döngüsü" kullanarak yapınız.
---
+++ Patika-UsluSayilar/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz: ");
+        int n = input.nextInt();
+        System.out.print("Üs Olacak Sayıyı Giriniz: ");
+        int k = input.nextInt();
+        int total = n;
+        for (int i = 0; i <= k; i++){
+            total *=n;
+            i++;
+        }
+        System.out.println(n + " üssü " + k + " = " + total);
+    }
+}
