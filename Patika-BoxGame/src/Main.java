d2cd73bfed55b41ecd58d314ed3fbc71269acdff
Tolga Gül <tolgagl19@gmail.com>
2022-12-22 00:35:19 +0300

    no message

--- Patika-BoxGame/src/Main.java
+++ Patika-BoxGame/src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Jackson", 65, 100, 98, 56);
+        Fighter f2 = new Fighter("Rousso", 88, 100, 78, 71);
+
+        Ring match = new Ring(f1, f2, 68, 100);
+        match.run();
+    }
 }
